195f2ee93622bf9a5d10ab3b056f49f6b9f122e6
Sean Wilson <paintballsean@gmail.com>
2020-08-24 13:53:33 -0700

    Commit will add form input

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,35 @@ PlacesBeen.prototype.deletePlace = function(id) {
   return false;
 }
 
-//PlacesBeen.prototype.updatePlace = function(id, dest ) {}
+// placesBeen.updatePlace(1, "", "", "Spring", "")
+// let placesBeen = new PlacesBeen();
+// let place = new Places("germany", "berlin wall", "fall", "test");
+// let place2 = new Places("switzerland", "chocolate", "fall", "this is a peaceful place");
+// placesBeen.addPlace(place);
+// placesBeen.addPlace(place2);
+
+PlacesBeen.prototype.updatePlace = function(id, destLocation, destLandmarks, timeOfYear, destNotes) {
+  for (let i = 0; i < this.places.length; i++) {
+    if (this.places[i]) {
+      if(this.places[i].id == id) {
+        if(destLocation != "") {
+          this.places[i].destLocation = destLocation;
+        }
+        if(destLandmarks != "") {
+          this.places[i].destLandmarks = destLandmarks;
+        }
+        if(timeOfYear != "") {
+          this.places[i].timeOfYear = timeOfYear;
+        }
+        if(destNotes != "") {
+          this.places[i].destNotes = destNotes;
+        }
+        return true;
+      }
+    }
+  };
+  return false;
+}
 
 // Business Logic Locations
 
@@ -48,7 +76,10 @@ function Places(destLocation, destLandmarks, timeOfYear, destNotes) {
   this.destNotes = destNotes;
 }
 
-PlacesBeen.prototype.Places = function() {
+/* PlacesBeen.prototype.Places = function() {
   return this.destLocation + " " + this.destLandmarks + " " + this.timeOfYear + " " + this.destNotes;
-}
+} */
 // User Interface
+$(documnet).ready(function() {
+  $
+});
