93d5b77c8ca099964809bb3e8a2f2c62bebf66a7
Sean Wilson <paintballsean@gmail.com>
2020-08-24 15:22:46 -0700

    commit will add additonial lines
    Co-authored-by: Mike Manchee <mikemanchee@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -79,23 +79,25 @@ function Places(destLocation, destLandmarks, timeOfYear, destNotes) {
 }
 
 function newPlaces(destLocation, destLandmarks, timeOfYear, destNotes) {
+  let placesBeen = new PlacesBeen();
   let place = new Places(destLocation, destLandmarks, timeOfYear, destNotes);
   placesBeen.addPlace(place);
 
-  let last = placesBeen.currentId
+  let last = placesBeen.currentId;
 
-  let name = "<div class=col-3>" + destLandmarks + "</div>"
-  let land = "<div class=col-3>" + destLocation + "</div>"
-  let time = "<div class=col-3>" + destLocation + "</div>"
-  let note = "<div class=col-3>" + destLocation + "</div>"
+  let name = "<div class=col-3>" + destLocation + "</div>";
+  let land = "<div class=col-3>" + destLandmarks + "</div>";
+  let time = "<div class=col-3>" + timeOfYear + "</div>";
+  let note = "<div class=col-3>" + destNotes + "</div>";
+  let title = "<div onclick=showHide(" + last + ")>" + name + "</div>";
 
+  let result = title + "<div id=location" + last + " class=row><div class=col-3>" + name + land + time + note + "</div></div>";
 
+  return result;
+}
 
-
-  let result = "<div class=row><div class=col-3></div></div>"
-
-
-  return result
+function showHide(id) {
+  $("#location" + id).toggle();
 }
 /* PlacesBeen.prototype.Places = function() {
   return this.destLocation + " " + this.destLandmarks + " " + this.timeOfYear + " " + this.destNotes;
@@ -106,11 +108,13 @@ $(document).ready(function() {
     event.preventDefault();
     let input1 = $("input#destinationInput").val();
     let input2 = $("input#landmarksInput").val();
-    let input3 = $("timeInput").val();
-    let input4 = $("notesInput").val();
+    let input3 = $("input#timeInput").val();
+    let input4 = $("input#notesInput").val();
 
     let result = newPlaces(input1, input2, input3, input4);
-
+    alert(result);
     $("#destinationResult").html(result);
+
+    $("#destinationResult").toggle();
   });
 });
