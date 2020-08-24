c1ace321260c54817b4f841b27e7a687bc1b443c
Sean Wilson <paintballsean@gmail.com>
2020-08-24 11:48:11 -0700

    Commit adds variable definition.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,51 @@
-// Business Logic Locations
+// Business Logic PlacesBeen
 function PlacesBeen () {
   this.places;
   this.currentId = 0;
 }
 
-PlacesBeen.prototype.addPlaces
-// Business Logic Prototypes
+PlacesBeen.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places.push(places);
+}
 
+PlacesBeen.prototype.assignId = function(){
+  this.currentId +=1;
+  return this.currentId;
+}
 
+PlacesBeen.prototype.findPlace = function(id) {
+  for (let i = 0; i < this.places.length; i++) {
+    if (this.places[i]) {
+      if (this.places[i].id == id) {
+        return this.places[i];
+      }
+    }
+  };
+  return false:
+}
+
+PlacesBeen.prototype.deletePlace = function(id) {
+  for (let i=0; i < this.places.length; i++) {
+    if (this.places[i]) {
+      if (this.places[i].id == id) {
+        delete this.places[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+PlacesBeen.prototype.updatePlace = function(id, )
+
+// Business Logic Locations
+
+function Places(destLocation, destLandmarks, timeOfYear, destNotes) {
+  this.destLocation = destLocation;
+  this.destLandmarks = destLandmarks;
+  this.timeOfYear = timeOfYear;
+  this.destNotes = destNotes;
+}
 
 // User Interface
